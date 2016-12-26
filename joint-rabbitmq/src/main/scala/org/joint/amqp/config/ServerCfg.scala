7b4bd7f4af4bc5874854b3a9ed827e5fec263ace
cfcodefans <cf.chenfan@gmail.com>
2016-12-26 23:36:14 +0800

    ..

--- joint-rabbitmq/src/main/scala/org/joint/amqp/config/ServerCfg.scala
+++ joint-rabbitmq/src/main/scala/org/joint/amqp/config/ServerCfg.scala
@@ -1,12 +1,13 @@
 package org.joint.amqp.config
 
 import java.io.Serializable
-import javax.jdo.annotations.PersistenceCapable
+import javax.jdo.annotations.{Cacheable, PersistenceCapable}
 
 /**
   * Created by chenf on 2016/12/21.
   */
 @PersistenceCapable
+@Cacheable(false.toString)
 class ServerCfg extends Serializable {
 
 }
